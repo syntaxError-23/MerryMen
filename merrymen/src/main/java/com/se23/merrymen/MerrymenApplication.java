7284095e624babe61e5a2cb5c254a851d31fcadc
Ahmed Abshir <ahmed.abshir91@gmail.com>
2026-06-15 20:05:48 +0100

    Sets up spring project and adds railway database connection

---
+++ merrymen/src/main/java/com/se23/merrymen/MerrymenApplication.java
@@ -0,0 +1,13 @@
+package com.se23.merrymen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerrymenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerrymenApplication.class, args);
+	}
+
+}
---
+++ merrymen/src/test/java/com/se23/merrymen/MerrymenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se23.merrymen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerrymenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
